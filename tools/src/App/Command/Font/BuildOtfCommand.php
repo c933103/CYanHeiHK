8e122fb805978081c6055310bb3f2fbae8d16601
tamcy <tamcy@users.noreply.github.com>
2016-05-06 22:31:40 +0800

    font:build-otf: add option to apply hackish fix to the FontBBox of the generated otf.

--- tools/src/App/Command/Font/BuildOtfCommand.php
+++ tools/src/App/Command/Font/BuildOtfCommand.php
@@ -15,12 +15,14 @@ class BuildOtfCommand extends ContainerAwareCommand
         $this
             ->setName('font:build-otf')
             ->setDescription('Builds OTF font. Must run after font:build-final-cmap.')
-            ->addOption('weight', 'w', InputOption::VALUE_REQUIRED, 'Specify the weight to act upon', null);
+            ->addOption('weight', 'w', InputOption::VALUE_REQUIRED, 'Specify the weight to act upon', null)
+            ->addOption('fix-fontbbox', null, InputOption::VALUE_NONE, 'If supplied, apply hack to fix the exceptionally large FontBBox');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
+        $fixFontBBox = $input->getOption('fix-fontbbox');
 
         $shsDir = $this->getParameter('shs_dir');
         $buildDir = $this->getParameter('build_dir');
@@ -34,7 +36,7 @@ class BuildOtfCommand extends ContainerAwareCommand
 
         foreach ($this->getActionableWeights($input->getOption('weight')) as $weight) {
             $io->section('Building font for ' . $weight . ' weight');
-            $this->buildFont($io, $shsDir, $ids, $buildDir, $weight);
+            $this->buildFont($io, $shsDir, $ids, $buildDir, $weight, $fixFontBBox);
         }
     }
 
@@ -42,7 +44,8 @@ class BuildOtfCommand extends ContainerAwareCommand
                                $shsDirRoot,
                                $worksetIds,
                                $buildDirRoot,
-                               $weight)
+                               $weight,
+                               $fixFontBBox)
     {
         $fontInfoDir = $this->getAppDataDir() . '/fontinfo';
 
@@ -90,12 +93,17 @@ class BuildOtfCommand extends ContainerAwareCommand
 
         $this->runExternalCommand($io, sprintf('%s %s %s %s',
             $this->getAfdkoCommand('mergeFonts'),
-            $wBuildDir . '/all.ps',
+            $wBuildDir . ($fixFontBBox ? '/all.ps.tmp' : '/all.ps'),
             $wBuildDir . '/merged.ps',
             $wShsFontDir . '/cidfont.ps.OTC.TC'
         ));
 
-        // 3. Merge original cidfont.ps.OTC.TC with new glyph ps generated in previous step.
+        if ($fixFontBBox) {
+            // 3. Fix FontBBox.
+            $this->fixFontBBox($io, $wShsFontDir, $wNewFontInfoDir, $wBuildDir, $wBuildDir . '/all.ps.tmp', $wBuildDir . '/all.ps');
+        }
+
+        // 4. Merge original cidfont.ps.OTC.TC with new glyph ps generated in previous step.
         $io->section('Build final OTF');
 
         $otfPath = $buildDirRoot . DIRECTORY_SEPARATOR . 'CYanHeiHK-' . $weight . '.otf';
@@ -114,4 +122,104 @@ class BuildOtfCommand extends ContainerAwareCommand
         ));
     }
 
+    private function fixFontBBox(SymfonyStyle $io,
+                                 $wShsFontDir,
+                                 $wNewFontInfoDir,
+                                 $wBuildDir,
+                                 $inputFile,
+                                 $outputFile)
+    {
+        $io->section('Fixing font BBox');
+
+        // Generate a temporary file with ridiculously tall glyphs removed, to get the
+        // smaller bbox value. Wide glyphs shouldn't cause issue so isn't removed.
+        $bboxFixFile = $wBuildDir . '/bboxfix.ps';
+        $this->runExternalCommand($io, sprintf('%s -cid %s %s %s',
+            $this->getAfdkoCommand('mergeFonts'),
+            $wNewFontInfoDir . '/cidfontinfo.OTC.TC',
+            $bboxFixFile,
+            sprintf('%1$s/bboxfix.map %1$s/bboxfix.pfa', $this->getWorksetDir(0) . '/Shared')
+        ));
+
+        $tmpPsFile = $wBuildDir . '/all_sans_tall_glyphs.ps';
+
+        $this->runExternalCommand($io, sprintf('%s %s %s %s',
+            $this->getAfdkoCommand('mergeFonts'),
+            $tmpPsFile,
+            $wBuildDir . '/bboxfix.ps',
+            $wShsFontDir . '/cidfont.ps.OTC.TC'
+        ));
+
+        // Determine the correct BBOx, logic ported from
+        // https://github.com/adobe-type-tools/perl-scripts/blob/master/Scripts/fix-fontbbox.pl
+        $llx = $urx = 500;
+        $lly = $ury = 380;
+
+        $logfile = $tmpPsFile . '.afm_log';
+        $this->runExternalCommand($io, 'tx -afm ' . $tmpPsFile . '  > ' . $logfile);
+
+        if (!file_exists($logfile)) {
+            throw new \Exception('Unexpected file not found: ' . $logfile);
+        }
+
+        $originalBbox = null;
+        foreach (file($logfile) as $line) {
+            if (preg_match('/FontBBox (.+)/', $line, $matches)) {
+                $originalBbox = trim($matches[1]);
+                $io->text('Original FontBBox: ' . $matches[1]);
+            }
+
+            if (preg_match('/^C\s+.+;\s+N\s+.+\s+;\s+B\s+(.+)\s+;/', $line, $matches)) {
+                list($a, $b, $c, $d) = explode(' ', $matches[1]);
+                if ($a < $llx) {
+                    $llx = $a;
+                }
+                if ($b < $lly) {
+                    $lly = $b;
+                }
+                if ($c > $urx) {
+                    $urx = $c;
+                }
+                if ($d > $ury) {
+                    $ury = $d;
+                }
+            }
+        }
+
+        $io->text(" Correct FontBBox: $llx $lly $urx $ury");
+
+        if (!$originalBbox) {
+            throw new \Exception('Original FontBBox information not found!');
+        }
+
+        $ifp = fopen($inputFile, 'r');
+        $ofp = fopen($outputFile, 'w');
+
+        $fontBboxReplaced = false;
+
+        $io->text('');
+        $io->text('Writing file with new FontBBox information...');
+
+        while (!feof($ifp)) {
+            // very confident that the FontBBox definition exists within the first megabyte of the file
+            $content = fread($ifp, 1024 * 1024);
+
+            if (!$fontBboxReplaced) {
+                $oldBBox = 'FontBBox {' . $originalBbox . '}';
+                $pos = strpos($content, $oldBBox);
+                if ($pos !== false) {
+                    $newBbox = sprintf('FontBBox {%s %s %s %s}', $llx, $lly, $urx, $ury);
+                    $content = substr_replace($content, $newBbox, $pos, strlen($oldBBox));
+                    $fontBboxReplaced = true;
+                }
+            }
+
+            fwrite($ofp, $content);
+        }
+
+        fclose($ifp);
+        fclose($ofp);
+
+        $io->text('Done.');
+    }
 }
